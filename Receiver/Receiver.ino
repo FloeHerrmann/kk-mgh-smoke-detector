12049437c0619cb50129ce5e82d4762a082c947e
Florian Herrmann <florian.herrmann@m2m-systems.de>
2015-02-02 11:16:02 +0100

    Erste Version der Empfänger-Software

---
+++ Receiver/Receiver.ino
@@ -0,0 +1,16 @@
+#include <Streaming.h>
+
+void setup(){
+
+	// Serielle Schnittstelle öffnen
+	Serial.begin( 115200 );
+
+	Serial << "* * * * * * * * * * * * * * * * * * * * * * * * * * * * *" << endl;
+	Serial << " Intelligenter Rauchmelder - Empfangsstation" << endl;
+	Serial << "* * * * * * * * * * * * * * * * * * * * * * * * * * * * *" << endl;
+
+}
+
+void loop() {
+
+}
