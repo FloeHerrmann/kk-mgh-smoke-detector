e8488100f07199feda35ded10fd96639e70f9001
Florian Herrmann <florian.herrmann@m2m-systems.de>
2015-02-02 14:40:52 +0100

    WiFi Einstellungen aus Datei auslesen
    - Bibliothek für .ini Dateien hinzugefügt
    - Update der "config.ini"
    - Einlesen der WiFi Einstellungen aus der "config.ini"

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,9 +1,19 @@
 #include <SPI.h>
 #include <SD.h>
+#include <IniFile.h>
+#include <Ethernet.h>
 #include <Streaming.h>
 #include <Watchdog.h>
 
 #define SD_CARD_SELECT 4
+
+char WiFiSSID[32] = "";
+char WiFiPassphrase[32] = "";
+char WiFiSecurity[40] = "";
+
+// Globaler Buffer zur anwendungsweiten Verwendung
+#define ApplicationBufferSize 200
+char ApplicationBuffer[ ApplicationBufferSize ];
 
 void setup(){
 
@@ -25,6 +35,9 @@ void setup(){
 		Serial << F( "[" ) << millis() << F( "] ") << F( "Datei 'config.ini' vorhanden..." );
 		if( SD.exists( "config.ini") ) {
 			Serial << F( "OK" ) << endl;
+			
+			bool isLoaded = SDCardLoadConfiguration();
+
 		} else {
 			Serial << F( "FEHLER" ) << endl;
 		}
@@ -48,21 +61,75 @@ void loop() {
 
 // Verbindung zur SD Karte initialisieren
 bool SDCardInitialize(){
+
+	WatchdogReset();
+
 	Serial << F( "[" ) << millis() << F( "] ") << F( "SD Karte initialisieren..." );
 
 	bool isAvailable = SD.begin( SD_CARD_SELECT );
 
 	if( isAvailable == true ) {
-	
 		Serial << F( "OK" ) << endl;
 		return true;
-	
 	} else {
-	
 		Serial << F( "FEHLER" ) << endl;
 		return false;
-	
 	}
+}
+
+// * * * Load configuration from emodul5.ini * * *
+bool SDCardLoadConfiguration() {
+
+	WatchdogReset();
+
+	IniFile configFile = IniFile( "config.ini" );
+
+	Serial << F( "[" ) << millis() << F( "] " ) << F( "Datei 'config.ini' öffnen..." );
+
+	if( configFile.open() ) {
+
+
+		Serial << F( "OK" ) << endl;
+		Serial << F( "[" ) << millis() << F( "] " ) << F( "Inhalt der 'config.ini' ist vailde..." );
+
+		if( configFile.validate( ApplicationBuffer , ApplicationBufferSize ) ) {
+			Serial << F( "OK" ) << endl;
+
+			if( configFile.getValue( "WIFI" , "ssid" , ApplicationBuffer , ApplicationBufferSize ) ){
+				strcpy( WiFiSSID , ApplicationBuffer );
+				Serial << F( "[" ) << millis() << F( "] " ) << F( "WiFi SSID: " ) << WiFiSSID << endl;
+			} else {
+				Serial << F( "FEHLER (SSID)" ) << endl;
+				return false;
+			}
+
+			if( configFile.getValue( "WIFI" , "pass" , ApplicationBuffer , ApplicationBufferSize ) ){
+				strcpy( WiFiPassphrase , ApplicationBuffer );
+				Serial << F( "[" ) << millis() << F( "] " ) << F( "WiFi Passphrase: " ) << WiFiPassphrase << endl;
+			} else {
+				Serial << F( "FEHLER (PASS)" ) << endl;
+				return false;
+			}
+
+			if( configFile.getValue( "WIFI" , "sec" , ApplicationBuffer , ApplicationBufferSize ) ){
+				strcpy( WiFiSecurity , ApplicationBuffer );
+				Serial << F( "[" ) << millis() << F( "] " ) << F( "WiFi Security: " ) << WiFiSecurity << endl;
+			} else {
+				Serial << F( "FEHLER (SEC)" ) << endl;
+				return false;
+			}
+
+			return true;
+		} else {
+			Serial << F( "FEHLER (VALIDIERUNG)" ) << endl;
+			return false;
+		}
+	} else {
+		Serial << F( "FEHLER (ÖFFNEN)" ) << endl;
+		return false;
+	}
+
+	return true;
 }
 
 // Watchdog starten
